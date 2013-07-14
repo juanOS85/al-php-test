e231adcc9ca8992614dba154eb0933aaf5c217cf
Juan Obando <juanchopx2@gmail.com>
2013-07-14 12:09:44 -0500

    Implemented basic TMDb API class. Modify inc folder structure.

---
+++ inc/tmdb_api_impl.php
@@ -0,0 +1,10 @@
+<?php
+/**
+* @author juanchopx2
+*/
+require dirname(__FILENAME__) . '/classes/TMDbAPI.php';
+
+$tmdbApi = new TMDbAPI('6425ff98fc0c954273045edc360b9e77');
+// $person = $tmdbApi->searchPerson('brad pitt');
+// $credits = $tmdbApi->searchPersonCredits($person->results[0]->id);
+?>
