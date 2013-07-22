2d7ebb896089757e53607f4346a3280862b9c1d4
Juan Obando <juanchopx2@gmail.com>
2013-07-21 20:59:20 -0500

    Setup initial app folder structure.  Included Twig-Bridge dependency to composer.json.

---
+++ web/index.php
@@ -0,0 +1,17 @@
+<?php
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$app = new Silex\Application();
+$app['debug'] = true;
+
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__ . '/../views'
+));
+
+// definitions
+$app->get('/', function () use ($app) {
+    return $app['twig']->render('index.twig');
+});
+
+$app->run();
+?>
