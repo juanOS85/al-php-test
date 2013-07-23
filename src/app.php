eb756e9aa4b921e790913592810a0b05493b9059
Juan Obando <juanchopx2@gmail.com>
2013-07-23 13:53:06 -0500

    Started assets folder structure.

--- src/app.php
+++ src/app.php
@@ -6,12 +6,12 @@ $app['debug'] = true;
 
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__ . '/../views',
-    'twig.options' => array('cache' => __DIR__.'/../cache')
+    'twig.options' => array('cache' => __DIR__ . '/../cache')
 ));
 
 // definitions
 $app->get('/', function () use ($app) {
-    return $app['twig']->render('index.twig');
+    return $app['twig']->render('index.html.twig');
 });
 
 return $app;
