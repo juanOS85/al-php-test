3e71025d7b4c556061a55beedc7eb232081b996d
Juan Obando <juanchopx2@gmail.com>
2013-07-23 21:57:39 -0500

    Added Symfony Form and Translation bundles.  Added custom style.  Separate form in a new template file.

--- src/app.php
+++ src/app.php
@@ -4,14 +4,32 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $app = new Silex\Application();
 $app['debug'] = true;
 
+// Registered serives
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__ . '/../views',
     'twig.options' => array('cache' => __DIR__ . '/../cache')
 ));
+$app->register(new Silex\Provider\FormServiceProvider());
+$app->register(new Silex\Provider\TranslationServiceProvider());
 
 // definitions
 $app->get('/', function () use ($app) {
-    return $app['twig']->render('index.html.twig');
+    $form = $app['form.factory']->createBuilder('form')
+        ->add('keyword', null, array(
+            'label' => false,
+            'attr'  => array('class' => 'span8')
+        ))
+        ->add('criteria', 'choice', array(
+            'choices' => array(
+                'actor' => 'Actor',
+                'movie' => 'Movie'
+            ),
+            'label' => false,
+            'attr'  => array('class' => 'span2')
+        ))
+        ->getForm();
+
+    return $app['twig']->render('index.html.twig', array('form' => $form->createView()));
 });
 
 return $app;
